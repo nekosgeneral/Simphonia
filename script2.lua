1e6b52c12f4a80aa7ede946f0084b8c0466c3209
nekosgeneral <74906549+nekosgeneral@users.noreply.github.com>
2024-12-26 02:20:31 +0200

    Create script2.lua

---
+++ script2.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet(\"https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/main/UNCCheckEnv.lua\
+local UIStroke = Instance.new("UIStroke")UIStroke.Color = Color3.fromRGB(100,0,100) UIStroke.Parent = FOVFFrame UIStroke.Thickness = 1 UIStroke.ApplyStrokeMode = "Border" game:GetService("StarterGui"):SetCore("SendNotification", {Title = "UNC TEST", Text = "Check console for information"})
